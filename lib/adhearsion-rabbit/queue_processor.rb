232cde021e05e13ac61862d0342e2d0d036c2db8
raeno <just.raeno@gmail.com>
2013-10-08 15:51:34 +0400

    Fixed wrong config namespace

--- lib/adhearsion-rabbit/queue_processor.rb
+++ lib/adhearsion-rabbit/queue_processor.rb
@@ -7,7 +7,7 @@ module AdhearsionRabbit
 		def start(queue_name)
 		    @queue = @channel.queue(queue_name)
 		    @exchange = @channel.default_exchange
-		    @subscriber = Adhearsion.config[:subscriber]
+		    @subscriber = Adhearsion.config.adhearsion_rabbit[:subscriber]
 
 		    @queue.subscribe(:block => true) do |delivery_info, properties, payload|
 		      @subscriber.process_message delivery_info, properties, payload
--- lib/adhearsion-rabbit/service.rb
+++ lib/adhearsion-rabbit/service.rb
@@ -11,17 +11,17 @@ module AdhearsionRabbit
 
 		def bunny_config
 			{ 
-				:host     => Adhearsion.config[:ip],
-	            :user     => Adhearsion.config[:login],
-	            :password => Adhearsion.config[:password],
-	            :vhost 	  => Adhearsion.config[:vhost]
+				:host     => Adhearsion.config.adhearsion_rabbit[:ip],
+	            :user     => Adhearsion.config.adhearsion_rabbit[:login],
+	            :password => Adhearsion.config.adhearsion_rabbit[:password],
+	            :vhost 	  => Adhearsion.config.adhearsion_rabbit[:vhost]
 	        }
 		end
 
 		def start
 			begin
 			    @queue_processor = QueueProcessor.new(@channel)
-			    queue_name = Adhearsion.config[:queue_name]
+			    queue_name = Adhearsion.config.adhearsion_rabbit[:queue_name]
 				@queue_processor.start(queue_name)
 			rescue Interrupt => _
 			  @channel.close
